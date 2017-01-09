edea8b50de1ea942a826a85d415e671a3b6042be
sahiljain112 <sahiljain112@gmail.com>
2017-01-09 11:17:46 +0530

    Basic template

---
+++ 02-Hotel-Booking/bookingOptions.js
@@ -0,0 +1,12 @@
+/*
+  Hotel Booking example
+
+  1) User can input the number of rooms to book. Price calculation is done accordingly
+  2) User can sort the options in increasing or decreasing order of price or name
+*/
+
+(function () {
+  console.log('Inside booking options JS')
+
+  
+})()
